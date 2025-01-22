d8be8f53a4dd81c5164b3c4205d8cabea3bc7659
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-22 16:50:36 -0600

    Initial Commit

---
+++ MagicPaperTests/MagicPaperTests.swift
@@ -0,0 +1,17 @@
+//
+//  MagicPaperTests.swift
+//  MagicPaperTests
+//
+//  Created by Isaac Urbina on 1/22/25.
+//
+
+import Testing
+@testable import MagicPaper
+
+struct MagicPaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
