8c5bc37e8de9827ac3e661967713a4e4c7653a46
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-22 17:34:13 -0600

    394. [Challenge] Detecting Images in AR (#1)

--- MagicPaper/ViewController.swift
+++ MagicPaper/ViewController.swift
@@ -10,65 +10,71 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+	
+	// MARK: - IBOutlets
+	
+	@IBOutlet var sceneView: ARSCNView!
+	
+	
+	// MARK: - UIViewController
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Set the view's delegate
+		sceneView.delegate = self
+		
+		// Show statistics such as fps and timing information
+		sceneView.showsStatistics = true
+		
+		let configuration = ARImageTrackingConfiguration()
+		
+		if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "NewsPaperImages", bundle: Bundle.main) {
+			
+			configuration.trackingImages = trackedImages
+			configuration.maximumNumberOfTrackedImages = 1
+			
+			print("Images found")
+		}
+		
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		// Create a session configuration
+		let configuration = ARWorldTrackingConfiguration()
+		
+		// Run the view's session
+		sceneView.session.run(configuration)
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		
+		// Pause the view's session
+		sceneView.session.pause()
+	}
+	
+	
+	// MARK: - ARSCNViewDelegate
+	
+	func renderer(_ renderer: any SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+		let node = SCNNode()
+		
+		if let imageAnchor = anchor as? ARImageAnchor {
+			let plane = SCNPlane(
+				width: imageAnchor.referenceImage.physicalSize.width,
+				height: imageAnchor.referenceImage.physicalSize.height
+			)
+			plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+			
+			let planeNode = SCNNode(geometry: plane)
+			planeNode.eulerAngles.x = -.pi / 2
+			
+			node.addChildNode(planeNode)
+		}
+		
+		return node
+	}
 }
